dbea70b6285470f5cec23f2e858ec1b0b4f381c7
Milo Bonnet <milovtt@gmail.com>
2022-12-21 13:47:01 +0100

    Start front-end

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const apiKey = "49e1ee2816ba179df29ad03d6797358a";
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+  
