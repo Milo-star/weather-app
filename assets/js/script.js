fe65f32f3510cd6a6fc76710beed7fdeccf86db8
Milo Bonnet <milovtt@gmail.com>
2022-12-22 15:16:18 +0100

    Finish Front + back

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,64 @@
-const apiKey = "49e1ee2816ba179df29ad03d6797358a";
+const newName = document.getElementById('cityInput');
+const btn = document.getElementById('button');
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
+newName.addEventListener("keypress", function(event){
+    if (event.key === "Enter"){
+        event.preventDefault();
+        btn.click;
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+function GetInfo () {
   
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + newName.value + '&appid=49e1ee2816ba179df29ad03d6797358a')
+    .then(response => response.json())
+    .then(data => {
+      for (i = 0; i < 5; i++) {
+        document.getElementById('day' + (i + 1) + 'Min').innerHTML = 'Min: ' + Number(data.list[i].main.temp_min - 273.15).toFixed(1) + '°'
+      }
+
+      for (i = 0; i < 5; i++) {
+        document.getElementById('day' + (i + 1) + 'Max').innerHTML = 'Max: ' + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + '°'
+      }
+
+      for (i = 0; i < 5; i++) {
+        document.getElementById('img' + (i + 1)).src = 'http://openweathermap.org/img/wn/' +
+        data.list[i].weather[0].icon +
+        '.png'
+      }
+      console.log(data)
+
+      document.getElementById('weatherContainer').style.display = "block";
+    })
+
+    .catch(err => alert('Veuillez mettre un nom de ville valide !'));
+
+    
+}
+
+const d = new Date()
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+function CheckDay (day) {
+  if (day + d.getDay() > 6) {
+    return day + d.getDay() - 7
+  } else {
+    return day + d.getDay()
+  }
+}
+
+for (i = 0; i < 5; i++) {
+  document.getElementById('day' + (i + 1)).innerHTML = weekday[CheckDay(i)]
+}
